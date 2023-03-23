e52d967a7707d0a5a63e01c2346f5f0234f169d5
Jun <dbapjh@gmail.com>
2023-03-23 18:08:52 +0900

    3-21수업

--- hr.sql
+++ hr.sql
@@ -46,3 +46,53 @@ from employees
 where hire_date between '2004-01-01' and '2004-12-31'
 order by hire_date;
 
+
+select last_name, salary 
+from employees
+where department_id in(20,50) and salary between 5000 and 12000
+order by salary;
+
+
+desc employees;
+
+select employee_id, last_name
+from employees
+where last_name like '%u%';
+
+select employee_id, hire_date
+from employees
+where hire_date like '04%';
+
+select last_name, job_id
+from employees
+where manager_id is null;
+
+select distinct department_id
+from employees
+where job_id='ST_CLERK' and department_id is not null;
+
+select employee_id, first_name, job_id, salary*commission_pct commission
+from employees
+where commission_pct is not null;
+
+select employee_id, first_name, hire_date, replace(job_id,'IT_PROG','프로그래머')
+from employees
+where department_id in (60,70,80,90);
+
+select employee_id, first_name || last_name, department_id,job_id
+from employees
+where job_id in ('AD_PRES', 'PU_CLERK');
+
+select last_name, salary,
+    decode(trunc(salary/2000,0),0,0.00,1,0.09,2,0.20,3,0.30,4,0.40,5,0.42,6,0.44,0.45) tax_rate
+from employees
+where department_id = 80;
+
+select max(salary) - min(salary)
+from employees;
+select *
+from employees;
+select count(distinct manager_id)
+from employees;
+
+
--- scott.sql
+++ scott.sql
@@ -115,3 +115,235 @@ from emp;
 select *
 from emp
 where sal between 2000 and 3000;
+
+
+
+select *
+from emp
+where ename like 'S%';
+
+select *
+from emp
+where ename like '_L%';
+
+
+select *
+from emp
+where ename not like '%AM%';
+
+select empno, ename, sal, deptno
+from emp
+--where deptno = 10
+intersect
+select empno, ename, sal, deptno
+from emp
+where deptno = 20;
+
+select *
+from emp
+where ename like '%S';
+
+select empno, ename, sal
+from emp
+where deptno=30 and job = 'SALESMAN'
+order by sal desc;
+
+select empno, ename, sal, deptno
+from emp
+where deptno in(20,30)
+intersect
+select empno, ename, sal, deptno
+from emp
+where sal > 2000;
+
+--upper: 모두 대문자, lower: 모두 소문자, initcap: 첫 글자만 대문자
+
+--dual : sys가 소유하는 테이블(임시 연산이나 함수의 결과 값 확인하는 용도
+--lengthb: byte 수
+select length('한글'), lengthb('한글'), lengthb('ab')
+from dual;
+
+--substr(문자열데이터, 시작위치, 추출길이): 문자열 일부 추출, 추출길이 생략 가능
+
+select job, substr(job,1,2), substr(job,3,2)
+from emp;
+
+--instr: 문자열 데이터 안에서 특정 문자 위치 찾기
+--instr(대상문자열,위치를 찾으려는 문자열, 대상문자열에서 찾기를 시작할 위치(선택), 시작위치에서 찾으려는 문자가 몇번째인지 지정(선택))
+
+select instr('hello, oracle!', 'l') in1, instr('hello, oracle!', 'l',5) in2,  instr('hello, oracle!', 'l',2,2) in3
+from dual;
+
+--replace: 특정 문자를 다른 문자로 변경
+--replace(문자열데이터, 찾는문자, 변경문자)
+
+select '010-1234-5678' 변경전, replace('010-1234-5678','-')
+from dual;
+
+--concat : 문자열 합치기
+
+-- || : 문자열 연결 연산자
+select empno || ename, empno || ':' || ename
+from emp;
+
+--trim, ltrim, rtrim : 공백제거
+select '   이것이    이것이   ' from dual;
+select trim('   이것이    이것이   ') from dual;
+
+
+--round, trunc, ceil, floor, mod
+
+
+--round: 반올림
+--trunc: 버림
+--ceil: 입력된 숫자와 가까운 큰 정수
+--floor: 입력된 숫자와 가까운 작은 정수
+--mod: 나머지
+select
+    round(1234.5678) as round,
+    round(1234.5678, 0) as round,
+    round(1234.5678, 1) as round,
+    round(1234.5678, 2) as round,
+    round(1234.5678, -1) as round,
+    round(1234.5678, -2) as round
+from dual;
+
+select mod(15,6) from dual;
+
+
+--날짜 함수
+--날짜 데이터 + 숫자 : 날짜 데이터보다 숫자만큼 일수 이후의 날짜
+--날짜 데이터 - 날짜 데이터 : 두 날짜 데이터 간의 일수 차이
+
+--sysdate 함수
+select 
+    sysdate,
+    sysdate -1 yesterday,
+    sysdate +1 tomorrow
+from dual;
+
+--add_months(날짜, 더할 개월수)
+
+select empno, ename, hiredate, add_months(hiredate, 50*12) 입사50주년날짜
+from emp;
+
+select empno, ename, hiredate
+from emp
+where months_between(sysdate, hiredate) < 540;
+
+select sysdate, add_months(sysdate, 6)
+from dual;
+
+select sysdate, next_day(sysdate, '금요일'), last_day(sysdate)
+from dual;
+
+--cc : 네 자리 연도의 끝 두자리를 기준으로 사용
+select 
+    sysdate, 
+    round(sysdate, 'CC') format_cc, 
+    round(sysdate, 'YYYY') format_YYYY,
+    round(sysdate, 'DDD') format_DDD,
+    round(sysdate, 'HH') format_HH
+from dual;
+
+
+--형변환 함수 : 자료형을 형 변환
+--NUMBER, VARCHAR2, DATE
+select empno, ename, empno+'500'
+from emp
+where ename = 'FORD';
+
+select to_char(sysdate, 'YYYY/MM/DD HH24:MI:SS') 현재날짜시간
+from dual;
+
+--to_number(문자열데이터, 인식될 숫자형태)
+
+select to_number('1,300','999,999') - to_number('1,500', '999,999')
+from dual;
+
+--L(local) 지역 화폐 단위 기호 붙여줌
+select sal, to_char(sal, '$999,999') sal_$, to_char(sal, 'L999,999') sal_L
+from emp;
+
+--to_date(문자열데이터, '인식될 날짜 형태')
+select to_date('2018-07-14', 'YYYY-MM-DD') TODATE1, to_date('20230320', 'YYYY-MM-DD') todate2
+from dual;
+
+--널처리 함수
+--NULL + 300 => NULL
+
+--NVL(데이터, 널일 경우 반환할 데이터)
+select empno, ename, sal, comm, sal+comm, nvl(comm,0)
+from emp;
+
+--NVL2(데이터, 널이 아닐 경우, 널일 경우)
+select empno, ename, sal, comm, sal+comm, nvl2(comm,1,0)
+from emp;
+
+--DECODE함수 / CASE 문
+
+--DECODE(검사 대상이 될 데이터,
+--       조건1, 조건1이 일치할때 실행할 구문
+--       조건2, 조거2가 일치할때 실행할 구문)
+
+select empno, ename,job, sal, decode(job, 'MANAGER',sal*1.1,'SALESMAN', sal*1.05, 'ANALYST', sal, sal*1.03) upsal
+from emp;
+
+select empno, ename, job, sal,
+    case job
+    when 'MANAGER' then sal*1.1
+    when 'SALESMAN' then sal*1.05
+    when 'ANAYLST' then sal
+    else sal*1.03
+    end upsal
+from emp;
+
+
+select empno, ename, sal, trunc(sal/21.5,2) DAY_PAY, round(trunc(sal/21.5,2)/8,1) TIME_PAY
+from emp;
+
+select empno, ename, hiredate, next_day(add_months(hiredate,3),'월요일'), nvl(to_char(comm), 'N/A')
+from emp;
+
+select *
+from emp;
+
+select empno, ename, mgr, decode(substr(to_char(mgr),1,2), null, '0000', '75', '5555', '76', '6666', '77', '7777',substr(to_char(mgr),1)) chg_mgr
+from emp;
+
+--다중행 함수 : sum, count, max, min, avg
+
+select sum(sal)
+from emp;
+
+select sum(distinct sal), sum(all sal), sum(sal)
+from emp;
+
+-- group by : 결과 값을 원하는 열로 묶어 출력
+
+select avg(sal) from emp where deptno=10;
+select avg(sal) from emp where deptno=20;
+select avg(sal) from emp where deptno=30;
+
+select avg(sal), deptno from emp group by deptno;
+
+select deptno, avg(comm) from emp group by deptno;
+
+select deptno, job, avg(sal) from emp group by deptno, job having avg(sal) >= 2000 order by deptno, job;
+
+
+select deptno, trunc(avg(sal)) avg_sal, max(sal) max_sal, min(sal) min_sal, count(deptno)
+from emp
+group by deptno
+order by deptno desc;
+
+select job, count(*)
+from emp
+group by job
+having count(*) >=3;
+
+select * from emp;
+
+select TO_CHAR(HIREDATE, 'YYYY') hire_year, deptno, count(*) cnt
+from emp
+group by TO_CHAR(HIREDATE, 'YYYY'), DEPTNO;
